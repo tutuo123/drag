42898035d8b435eb7c9186bf820f393d65b9443b
tutuo123 <wangyingchao@163.com>
2016-05-06 22:15:41 +0800

    create text

---
+++ js/drag.js
@@ -0,0 +1,11 @@
+/**
+ * Created by wangyingchao on 16/5/6.
+ */
+function drag(){
+    var oDiv1 = document.getElementById('div1');
+    var disX = 0;
+    var disY = 0;
+    oDiv1.onmosedown = function () {
+        alert(1);
+    }
+}
