6835d6024638c1f681649c40508ce1a703df9683
tutuo123 <wangyingchao@163.com>
2016-05-06 15:30:48 +0800

    change2

--- drag.js
+++ drag.js
@@ -2,5 +2,8 @@
  * Created by wangyingchao on 16/5/6.
  */
 function drag(){
-    
+    var oDiv1 = document.getElementById('div1');
+    oDiv1.onmosedown = function () {
+        alert(1);
+    }
 }
